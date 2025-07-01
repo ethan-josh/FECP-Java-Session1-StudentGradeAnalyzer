c3d6237d71fb40b11b98989d6d901fe9f4d1cc9c
Ethan Joshua Camingao <ethanjoshua.camingao@GXCHLAP00388.local>
2025-07-01 10:08:29 +0800

    feat: getting number of students

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,19 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
+        Scanner myObj = new Scanner (System.in);
+
+        ArrayList<Integer> studentScores = new ArrayList<>();
+        ArrayList<String> studentNames = new ArrayList<>();
+
+        // Getting the number of students
+        int studentCount = 0;
+        System.out.print("Enter number of students: ");
+        studentCount = myObj.nextInt();
+
+
+
+
 
     }
 }
